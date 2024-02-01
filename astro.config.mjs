4cba1553ba63e56ce1af54520c7f4c7f5c5e0ff2
Jimmy B <chips_xp@yahoo.com>
2024-01-31 21:38:48 -0500

    Big Easy Savings website built with Astrojs
    A menu navigation on a wide fluid background image through grid row and column layout. Article content in the mid-section and Babylonjs window view to demonstrate 3d models.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from "astro/config";
+import mdx from "@astrojs/mdx";
+import { astroImageTools } from "astro-imagetools";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  experimental: {
+    assets: true
+  },
+  image: {
+    service: sharpImageService()
+  },
+  integrations: [mdx(), react()],
+  integrations: [astroImageTools]
+});
